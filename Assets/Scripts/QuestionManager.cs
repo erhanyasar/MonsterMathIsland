9e5beeac6e38ba72e4dd5eeaa4f9d8081ec9dc5c
Ben <ben@arrogantpixel.com>
2021-10-27 16:13:08 +0100

    week 1 scripting complete

---
+++ Assets/Scripts/QuestionManager.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+
+public class QuestionManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _questionTextField;
+    
+    private string _question;
+    [SerializeField] private float _answer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int operand1 = Mathf.FloorToInt(Random.Range(0,100));
+        int operand2 = Mathf.FloorToInt(Random.Range(0,100));
+
+        //Is it a + or - question, creates 50% chance
+        if (Random.value < 0.5f)
+        {
+            //using string concatenation
+            //_question = operand1 + "-" + operand2 + " = ? ";
+            
+            //using string interpolation
+            _question = $"{operand1} - {operand2} = ?";
+            _answer =  (operand1 - operand2);
+        }
+        else
+        {
+            //using string concatenation
+            //_question = operand1 + "+" + operand2 + " = ? ";
+            
+            //using string interpolation
+            _question = $"{operand1} + {operand2} = ?";
+            _answer =  (operand1 + operand2);
+        }
+
+        _questionTextField.text = _question;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
